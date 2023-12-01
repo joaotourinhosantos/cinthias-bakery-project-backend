a43a226b37db30244b8f1450db04e80cca131fea
João Tourinho <103600204+joaotourinhosantos@users.noreply.github.com>
2023-12-01 19:24:53 -0300

    Update 2023_12_01_034555_create_profiles_table.php
    fields

--- bakery-bd/database/migrations/2023_12_01_034555_create_profiles_table.php
+++ bakery-bd/database/migrations/2023_12_01_034555_create_profiles_table.php
@@ -16,9 +16,15 @@ return new class extends Migration
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
             $table->string('name', 255);
-            $table->integer('weight')->nullable();
-            $table->float('height');
-            $table->float('result');
+            $table->string('last_name', 255);
+            $table->integer('bday');
+            $table->string('gender', 255);
+            $table->string('address', 255);
+            $table->integer('number');
+            $table->string('city', 255);
+            $table->string('cep', 255);
+            $table->string('tel', 255);
+            $table->string('email', 255);
             $table->timestamps();
         });
     }
